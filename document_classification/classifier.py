c38381d9a1ffe3966b6e9223c40fdb7b5f249e1b
gor_lobs <nikita.lobosov.wa@yandex.ru>
2019-12-10 19:40:20 +0300

    Add classifiers_dict field to MultiClassifier class

--- document_classification/classifier.py
+++ document_classification/classifier.py
@@ -10,13 +10,23 @@ class MultiClassifier:
         self.vectorizer = TfidfVectorizer()
         self.vectorized_train_data = self.vectorizer.fit_transform(train_data)
 
-        self.bayes_classifier = MultinomialNB(alpha=.01)
-        self.bayes_classifier.fit(self.vectorized_train_data, train_target)
+        bayes_classifier = MultinomialNB(alpha=.01)
+        bayes_classifier.fit(self.vectorized_train_data, train_target)
 
-    def classify_text(self, text):
+        self.classifiers_dict = self.create_classifiers(train_target)
+    
+    def create_classifiers(self, train_target):
+        bayes_classifier = MultinomialNB(alpha=.01)
+        bayes_classifier.fit(self.vectorized_train_data, train_target)
+        return {
+            'mn_naive_bayes': bayes_classifier,
+        }
+
+    def classify_text(self, text, classifier_name):
+        classifier = self.classifiers_dict[classifier_name]
         text_data = np.array([text])
         vectorized_text = self.vectorizer.transform(text_data)
-        result_indices = self.bayes_classifier.predict(vectorized_text)
+        result_indices = classifier.predict(vectorized_text)
         return self.class_names[result_indices[0]]
 
 
@@ -27,5 +37,6 @@ if __name__ == '__main__':
                                  train_dataset.target_names)
 
     result = multiclassifier.classify_text(
-        'Pitcher. Championship. Nice strike. Great homerun.')
+        'Pitcher. Championship. Nice strike. Great homerun.',
+        'mn_naive_bayes')
     print(result)
