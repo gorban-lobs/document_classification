11900e11849324687ffeb366f0453733f61d3318
lobosov@mrbot.im <lobs.gorban@yandex.ru>
2019-12-12 00:50:26 +0300

    Add description to classification groups

--- document_classification/classifier.py
+++ document_classification/classifier.py
@@ -13,6 +13,8 @@ class MultiClassifier:
         self.vectorized_train_data = self.vectorizer.fit_transform(train_data)
 
         self.classifiers_dict = self.create_classifiers(train_target)
+
+        self.group_description = self.get_group_description()
     
     def create_classifiers(self, train_target):
         bayes_classifier = MultinomialNB(alpha=.01)
@@ -39,5 +41,30 @@ class MultiClassifier:
             text_data = np.array([text])
             vectorized_text = self.vectorizer.transform(text_data)
             result_indices = classifier.predict(vectorized_text)
-            return self.class_names[result_indices[0]]
+            return self.group_description[self.class_names[result_indices[0]]]
         return "Неправильный тип классификатора"
+
+
+    def get_group_description(self):
+        return {
+            'comp.graphics': 'Компьютерная графика',
+            'comp.os.ms-windows.misc': 'OS Windows',
+            'comp.sys.ibm.pc.hardware': 'IBM PC',
+            'comp.sys.mac.hardware': 'Macintosh',
+            'comp.windows.x': 'Windows X',
+            'rec.autos': 'Автомобили',
+            'rec.motorcycles': 'Мотоциклы',
+            'rec.sport.baseball': 'Бейсбол',
+            'rec.sport.hockey': 'Хоккей',
+            'sci.crypt': 'Криптография',
+            'sci.electronics': 'Электроника',
+            'sci.med': 'Медицина',
+            'sci.space': 'Космос',
+            'misc.forsale': 'Продажи',
+            'talk.politics.misc': 'Политика (разное)',
+            'talk.politics.guns': 'Политика (оружие)',
+            'talk.politics.mideast': 'Политика (Ближний Восток)',
+            'talk.religion.misc': 'Религия (разное)',
+            'alt.atheism': 'Атеизм',
+            'soc.religion.christian': 'Религия (христианство)',
+        }
